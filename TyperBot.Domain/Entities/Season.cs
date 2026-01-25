71c27a4416cb74f1007fef0d849314875d14dc2d
Zoladrian <zoladrian1@gmail.com>
2026-01-25 20:29:02 +0100

    Add PreferredTableFormat property to Season entity for enhanced table format management

--- TyperBot.Domain/Entities/Season.cs
+++ TyperBot.Domain/Entities/Season.cs
@@ -1,3 +1,5 @@
+using TyperBot.Domain.Enums;
+
 namespace TyperBot.Domain.Entities;
 
 public class Season
@@ -5,6 +7,7 @@ public class Season
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public bool IsActive { get; set; }
+    public TableFormat PreferredTableFormat { get; set; } = TableFormat.Text;
 
     // Navigation properties
     public ICollection<Round> Rounds { get; set; } = new List<Round>();
